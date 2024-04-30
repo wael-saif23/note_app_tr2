170fabe13dba23d717d75ade3c41061bd39149c4
wael-saif23 <ws.mywork@gmail.com>
2024-05-01 01:46:31 +0300

    Add Hive annotations to NoteModel class
    Import Hive package and add Hive annotations to NoteModel class for serialization
    and deserialization purposes. Update fields with HiveField annotations for proper
    mapping.

--- lib/models/note_model.dart
+++ lib/models/note_model.dart
@@ -1,7 +1,15 @@
-class NoteModel {
+import 'package:hive/hive.dart';
+part 'note_model.g.dart';
+
+@HiveType(typeId: 1)
+class NoteModel extends HiveObject {
+  @HiveField(0)
   final String title;
+  @HiveField(1)
   final String content;
+  @HiveField(2)
   final String date;
+  @HiveField(3)
   final int color;
 
   NoteModel(
