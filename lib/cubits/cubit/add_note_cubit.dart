839562e71ea092a2827754e8191d4a3acac78c85
wael-saif23 <ws.mywork@gmail.com>
2024-05-04 17:48:35 +0300

    Add functionality to add a note with error handling
    Added functionality to add a note to Hive database with error handling in the AddNoteCubit.

--- lib/cubits/cubit/add_note_cubit.dart
+++ lib/cubits/cubit/add_note_cubit.dart
@@ -1,10 +1,21 @@
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
 import 'package:note_app_tr2/models/note_model.dart';
+import 'package:hive/hive.dart';
+import 'package:note_app_tr2/constants.dart';
 
 part 'add_note_state.dart';
 
 class AddNoteCubit extends Cubit<AddNoteState> {
   AddNoteCubit() : super(AddNoteInitial());
-  addNote(NoteModel note){}
+  addNote(NoteModel note) {
+    emit(AddNoteLoading());
+    try {
+      var notesBox = Hive.box<NoteModel>(kNoteBox);
+    emit(AddNoteSuccess());
+    notesBox.add(note);
+    } catch (e) {
+      emit(AddNoteFailure(e.toString()));
+    }
+  }
 }
